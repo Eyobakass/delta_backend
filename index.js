3da6b177d9f1ff25d85517e37b07e646def43f82
Eyob <ekassaye0461@gmail.com>
2025-05-16 12:53:19 +0300

    organized the basic folder and file structures

---
+++ index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const app= express();
+
+
+require('./startup/config')();
+require('./startup/routes')(app);
+require('./startup/db')();
+
+
+
+const port=process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
