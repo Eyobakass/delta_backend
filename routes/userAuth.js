45e561e2f727532e389deaf43bbfb4b32a1fb1c2
Eyob <ekassaye0461@gmail.com>
2025-05-18 22:44:54 +0300

    changed the login form requirement from first and second name and password  to only email and password

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const Router = express.Router();
 const User = require("../models/user");
 Router.post("/", async (req, res) => {
-  const user = await User.findOne({ firstName: req.body.firstName, lastName: req.body.lastName });
+  const user = await User.findOne({ email:req.body.email });
   if (!user) return res.status(400).send("Invalid name or password");
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid name or password");
