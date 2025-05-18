a660792ee1a1da06dec4bcc4d82d9c01bf8eb8a2
Eyob <ekassaye0461@gmail.com>
2025-05-18 23:32:09 +0300

    added the cors package to avoid any port conflicts

--- startup/routes.js
+++ startup/routes.js
@@ -1,14 +1,21 @@
-const express=require("express");
-const {userRouter}=require("../routes/users");
-const {carRouter}=require("../routes/cars");
-const {bookingRouter}=require("../routes/bookings");
-const {reviewRouter}=require("../routes/reviews");
-const {userAuthRouter}=require("../routes/userAuth");
-module.exports=function(app){
-    app.use(express.json());
-    app.use("/api/users",userRouter);
-    app.use("/api/cars",carRouter);
-    app.use("/api/bookings",bookingRouter);
-    app.use("/api/reviews",reviewRouter);
-    app.use("/api/userAuth",userAuthRouter);
-}
+const express = require("express");
+const { userRouter } = require("../routes/users");
+const { carRouter } = require("../routes/cars");
+const { bookingRouter } = require("../routes/bookings");
+const { reviewRouter } = require("../routes/reviews");
+const { userAuthRouter } = require("../routes/userAuth");
+const cors = require("cors");
+module.exports = function (app) {
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: "*",
+      exposedHeaders: ["x-auth-token"],
+    })
+  );
+  app.use("/api/users", userRouter);
+  app.use("/api/cars", carRouter);
+  app.use("/api/bookings", bookingRouter);
+  app.use("/api/reviews", reviewRouter);
+  app.use("/api/userAuth", userAuthRouter);
+};
